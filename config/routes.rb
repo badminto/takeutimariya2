554182d7c63075fdeb525f8e979f3323f6c7a373
badminto <eight2019@outlook.jp>
2022-12-02 04:55:42 +0900

    ・ステップ1-3 が多分終了 これからプルリクにとりかかる

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index' # ここを追記します
+  get 'home/index' # 自動で設定されたルーティング
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
