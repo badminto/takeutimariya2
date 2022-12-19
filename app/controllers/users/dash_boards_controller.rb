68b6fcc7f9b77d96dafe2ab351d78f667eca3924
badminto <eight2019@outlook.jp>
2022-12-19 16:21:59 +0900

    1-5 おそらく完了 プルリク前

---
+++ app/controllers/users/dash_boards_controller.rb
@@ -0,0 +1,8 @@
+class Users::DashBoardsController < ApplicationController
+    before_action :authenticate_user!
+    def index
+    end
+  
+    def show
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index' # ここを追記します
   get 'home/index' # 自動で設定されたルーティング
+  namespace :users do
+  resource :dash_boards, only: :show, as: :root
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+end
 end
