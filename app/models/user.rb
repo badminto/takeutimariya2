c803b3183b353666dde8d9259e8449046dc15076
badminto <eight2019@outlook.jp>
2022-12-25 05:21:17 +0900

    1-6 記事一覧と記事詳細画面以外完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  has_many :articles, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
   validates :name, presence: true
 end
