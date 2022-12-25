f0bf1380a7c15c6e29bbab8d2a291b13e36c1445
badminto <eight2019@outlook.jp>
2022-12-26 04:30:14 +0900

    1-6 完了 ログアウト機能編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
     get 'articles/new'
   end
   
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index' # ここを追記します
   get 'home/index' # 自動で設定されたルーティング
